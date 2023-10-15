becb0e8c574a024d534372c44fd951fcc4daaade
Pranjal003 <sharmapranjal0403@gmail.com>
2023-10-15 15:34:58 +0530

    creating travelCompanion

---
+++ src/api/travelAdvisorAPI.js
@@ -0,0 +1,25 @@
+/* eslint-disable consistent-return */
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'X-RapidAPI-Key': '88990f14e3msh2d295ad849eb5f0p116fb6jsn02a1c4b6f3c2',
+        // 'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
