71b4021dd23af6181310a655ffe2b48553e5bfb2
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2023-11-01 16:24:07 +0530

    Update travelAdvisorAPI.js

--- src/api/travelAdvisorAPI.js
+++ src/api/travelAdvisorAPI.js
@@ -5,7 +5,7 @@ export const getPlacesData = async (type, sw, ne) => {
   try {
     const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
       params: {
-        bl_latitude: sw.lat,
+        bl_latitude: sw.lat, 
         bl_longitude: sw.lng,
         tr_longitude: ne.lng,
         tr_latitude: ne.lat,
