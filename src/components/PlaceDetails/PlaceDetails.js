6c63729d0a4fe567b848f0859eb7b9eabc257ff1
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2023-11-03 16:42:15 +0530

    Update PlaceDetails.js

--- src/components/PlaceDetails/PlaceDetails.js
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -7,7 +7,7 @@ import Rating from '@material-ui/lab/Rating';
 import useStyles from './styles.js';
 
 const PlaceDetails = ({ place, selected, refProp }) => {
-  if (selected) refProp?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  if (selected) refProp?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' }); 
   const classes = useStyles(); 
 
   return ( 
