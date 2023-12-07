50311e189d8be93bbffa332cbc99e03c444e575e
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2023-12-07 11:56:47 +0530

    Update List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -37,7 +37,7 @@ const List = ({ places, type, setType, rating, setRating, childClicked, isLoadin
               <MenuItem value="4">Above 4.0</MenuItem>
               <MenuItem value="4.5">Above 4.5</MenuItem>
             </Select>
-          </FormControl>
+          </FormControl> 
           <Grid container spacing={3} className={classes.list}>
             {places?.map((place, i) => (
               <Grid ref={elRefs[i]} key={i} item xs={12}>
