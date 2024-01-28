3f546dd0e7a14af408fca7eb6c426a8dc98daa6b
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2024-01-28 22:14:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
 
 import { getPlacesData } from './api/travelAdvisorAPI';
-import Header from './components/Header/Header';
+import Header from './components/Header/Header'; 
 import List from './components/List/List'; 
 import Map from './components/Map/Map';    
    
