7abd6ba937f23688b5830c4b44fe8091ef84af9a
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2024-01-31 23:52:39 +0530

    Update styles.js

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  formControl: {
+  formControl: { 
     margin: theme.spacing(1), minWidth: 120, marginBottom: '30px',
   },  
   selectEmpty: {
