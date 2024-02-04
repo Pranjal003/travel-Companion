b06c239561cc271d7ba4e2e744a87f91dd162bfe
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2024-02-04 16:20:31 +0530

    Update styles.js

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -5,7 +5,7 @@ export default makeStyles((theme) => ({
     margin: theme.spacing(1), minWidth: 120, marginBottom: '30px',
   },  
   selectEmpty: {
-    marginTop: theme.spacing(2), 
+    marginTop: theme.spacing(2),  
   }, 
   loading: { 
     height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center',
