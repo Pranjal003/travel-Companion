f9d1c50568e29bbb3a0271fed789e351e0801763
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2024-02-10 19:42:55 +0530

    Update List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -16,7 +16,7 @@ const List = ({ places, type, setType, rating, setRating, childClicked, isLoadin
     <div className={classes.container}>
       <Typography variant="h5">Discover, Dine, Explore, Enjoy!</Typography> 
       {isLoading ? ( 
-        <div className={classes.loading}>
+        <div className={classes.loading}> 
           <CircularProgress size="5rem" />
         </div>
       ) : (
