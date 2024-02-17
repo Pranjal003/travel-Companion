19794af74d5b03b2bf2cacf64dd1752bbfc3c587
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2024-02-17 21:42:14 +0530

    Update List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,7 +6,7 @@ import useStyles from './styles.js';
        
 const List = ({ places, type, setType, rating, setRating, childClicked, isLoading }) => {       
   const [elRefs, setElRefs] = useState([]);        
-  const classes = useStyles();      
+  const classes = useStyles();       
       
   useEffect(() => {     
     setElRefs((refs) => Array(places.length).fill().map((_, i) => refs[i] || createRef()));       
