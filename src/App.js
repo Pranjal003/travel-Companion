d3e08e2c7f665713b51fc95373727b550b71610b
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2024-03-07 21:39:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Map from './components/Map/Map';
 const App = () => {  
   const [type, setType] = useState('restaurants');     
   const [rating, setRating] = useState('');  
-   
+    
   const [coords, setCoords] = useState({});  
   const [bounds, setBounds] = useState(null);  
  
