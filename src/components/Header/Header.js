6f439d8243925d4e8b82593b50cd184d9e864c32
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2024-05-10 22:45:08 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
-import React from 'react';
+ import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search'; 
  
 import useStyles from './styles.js';        
      
-const Header = ({ onPlaceChanged, onLoad }) => {       
+const Header = ({ onPlaceChanged, onLoad }) => {        
   const classes = useStyles();          
   
   return (             
