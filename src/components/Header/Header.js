67c243f1ae91be58eef2aa33c3bfa424df91dc34
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2024-07-13 22:39:06 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ import useStyles from './styles.js';
        
 const Header = ({ onPlaceChanged, onLoad }) => {          
   const classes = useStyles();                
-        
+         
   return (                 
     <AppBar position="static">            
       <Toolbar className={classes.toolbar}>            
